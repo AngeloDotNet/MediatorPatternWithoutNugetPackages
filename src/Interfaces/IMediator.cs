604f445392c06f7de0818ab02f0955a994f81e10
Angelo Pirola <angelo9871@gmail.com>
2025-04-04 08:59:12 +0200

    Add IMediator and IRequestHandler interfaces
    Introduce `IMediator` for sending requests and `IRequestHandler` for handling requests, both within the `ExampleWithoutMediatR.Interfaces` namespace. The `IMediator` includes methods for single and response-returning requests, while `IRequestHandler` is defined generically for handling requests and responses.

---
+++ src/Interfaces/IMediator.cs
@@ -0,0 +1,7 @@
+﻿namespace ExampleWithoutMediatR.Interfaces;
+
+public interface IMediator
+{
+	Task Send<TRequest>(TRequest request);
+	Task<TResponse> Send<TRequest, TResponse>(TRequest request);
+}
---
+++ src/Interfaces/IRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace ExampleWithoutMediatR.Interfaces;
+
+public interface IRequestHandler<TRequest>
+{
+	Task Handle(TRequest request);
+}
+
+public interface IRequestHandler<TRequest, TResponse>
+{
+	Task<TResponse> Handle(TRequest request);
+}
