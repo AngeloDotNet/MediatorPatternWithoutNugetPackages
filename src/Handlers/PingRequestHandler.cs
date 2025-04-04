339d17842298a591b9a3a70de5f13344a93b560e
Angelo Pirola <angelo9871@gmail.com>
2025-04-04 08:59:05 +0200

    Add Ping and Pong request handlers
    Introduce `PingRequestHandler` and `PongRequestHandler` classes in the `ExampleWithoutMediatR.Handlers` namespace. The `PingRequestHandler` logs incoming `PingRequest` messages with the current UTC time, while the `PongRequestHandler` returns a formatted response for `PongRequest` messages. Both classes implement the `IRequestHandler` interface.

---
+++ src/Handlers/PingRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using ExampleWithoutMediatR.Interfaces;
+using ExampleWithoutMediatR.Requests;
+
+namespace ExampleWithoutMediatR.Handlers;
+
+public class PingRequestHandler : IRequestHandler<PingRequest>
+{
+	public Task Handle(PingRequest request)
+	{
+		Console.WriteLine($"Ping received with message: {request.Message} at " + DateTime.UtcNow);
+		return Task.CompletedTask;
+	}
+}
---
+++ src/Handlers/PongRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using ExampleWithoutMediatR.Interfaces;
+using ExampleWithoutMediatR.Requests;
+
+namespace ExampleWithoutMediatR.Handlers;
+
+public class PongRequestHandler : IRequestHandler<PongRequest, string>
+{
+	public Task<string> Handle(PongRequest request)
+	{
+		return Task.FromResult($"Pong response to message: {request.Message} at " + DateTime.UtcNow);
+	}
+}
