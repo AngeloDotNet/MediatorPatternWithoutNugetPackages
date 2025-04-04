2d2bd93ca8bd9f3018faf2340ac1725278cbde55
Angelo Pirola <angelo9871@gmail.com>
2025-04-04 08:59:37 +0200

    Add .NET 8.0 project setup with Mediator pattern
    - Updated `ExampleWithoutMediatR.csproj` to target .NET 8.0 and include `Microsoft.Extensions.DependencyInjection` for dependency injection.
    - Implemented `Main` method in `Program.cs` to configure services and demonstrate sending requests using `IMediator`.
    - Created/modified `Startup.cs` to register `IMediator` and request handlers for `PingRequest` and `PongRequest`.

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExampleWithoutMediatR.Interfaces;
+using ExampleWithoutMediatR.Requests;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExampleWithoutMediatR;
+
+class Program
+{
+	static async Task Main(string[] args)
+	{
+		var serviceCollection = new ServiceCollection();
+		var startup = new Startup();
+		startup.ConfigureServices(serviceCollection);
+
+		var serviceProvider = serviceCollection.BuildServiceProvider();
+		var mediator = serviceProvider.GetService<IMediator>();
+
+		var pingRequest = new PingRequest { Message = "Hello Ping!" };
+		await mediator.Send(pingRequest);
+
+		await Task.Delay(2000);
+
+		var pongRequest = new PongRequest { Message = "Hello Pong!" };
+		var response = await mediator.Send<PongRequest, string>(pongRequest);
+		Console.WriteLine(response);
+
+		Console.Read();
+	}
+}
---
+++ src/Startup.cs
@@ -0,0 +1,17 @@
+﻿using ExampleWithoutMediatR.Handlers;
+using ExampleWithoutMediatR.Interfaces;
+using ExampleWithoutMediatR.Requests;
+using ExampleWithoutMediatR.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExampleWithoutMediatR;
+
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddSingleton<IMediator, Mediator>();
+		services.AddTransient<IRequestHandler<PingRequest>, PingRequestHandler>();
+		services.AddTransient<IRequestHandler<PongRequest, string>, PongRequestHandler>();
+	}
+}
